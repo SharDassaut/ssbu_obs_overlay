6aaac5e189cda059767c696888c58f4a3b090eac
sharhabia <sharhabia@gmail.com>
2025-07-15 12:54:41 +0200

    Add very basic workign overlay/server/panel communication

--- panel.js
+++ panel.js
@@ -1,15 +1,27 @@
-const rni = document.getElementById("rni")
-const rnb = document.getElementById("rnb")
-const text = document.getElementById("text")
+const div = document.getElementById("match-data");
+const match_data = div.querySelectorAll("input");
+const btn = div.querySelector("button")
 
-rnb.addEventListener('click',()=>{
-    const inputValue = rni.value;
-    if(inputValue == ""){
-        console.log("EMPTY")
-    }
-    else{
-        console.log(inputValue)
-        text.innerText = inputValue
-        rni.value = ""
-    }
+btn.addEventListener('click',async ()=>{
+    let json = {}
+
+    match_data.forEach((e)=> {
+        if(e.value == "") return 
+        json[e.id] = e.value
+    })
+
+    await postData(json);
+
 })
+
+// re-read and understand this piece of cde
+async function postData(json) {
+  const response = await fetch('http://localhost:6355/api-match-data', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(json)
+  });
+
+  const data = await response.json();
+  console.log(data);
+}
